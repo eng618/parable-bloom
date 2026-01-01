b45c32d06af411d1b07fe270d9eb6e2bd163eb2b
Eric N. Garcia <eng618@garciaericn.com>
2026-01-01 14:02:20 -0500

    refactor(levels): remove legacy level schema

--- lib/features/game/domain/repositories/level_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/level.dart';
-
-abstract class LevelRepository {
-  Future<Level?> getLevel(int levelNumber);
-  Future<List<int>> getAvailableLevelNumbers();
-  Future<bool> levelExists(int levelNumber);
-}
